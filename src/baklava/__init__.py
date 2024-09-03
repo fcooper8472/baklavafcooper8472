7763046a258f752de28c7f036d11d574256cd30b
Fergus Cooper <fcooper8472@gmail.com>
2024-09-03 14:02:04 +0100

    Init with copier

---
+++ src/baklava/__init__.py
@@ -0,0 +1,9 @@
+"""Documentation about baklava."""
+
+import logging
+
+logging.getLogger(__name__).addHandler(logging.NullHandler())
+
+__author__ = "Fergus Cooper"
+__email__ = "fcooper8472@gmail.com"
+__version__ = "0.1.0"
